e877a1bdfe7925078d7fe42a57035f4bd40d2259
Dmitry Strizhakov <d.strizhakov@vk.team>
2024-01-26 13:18:10 +0300

    feat: implement copy files and folders

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,8 +1,46 @@
-import { path } from 'path';
-import { fs } from 'fs';
+import { log } from 'node:console';
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const fromFolder = path.join(__dirname, "files");
+const toFolder = path.join(__dirname, "files-copy");
+
+const copyFileOrFolder = async (source, destination) => {
+    const files = await fs.readdir(source);
+    for (const file of files) {
+        const sourceFilePath = path.join(source, file);
+        const destinationFilePath = path.join(destination, file);
+        const stat = await fs.stat(sourceFilePath);
+        if (stat.isDirectory()) {
+            await fs.mkdir(destinationFilePath)
+            await copyFileOrFolder(sourceFilePath, destinationFilePath);
+        } else {
+            await fs.copyFile(sourceFilePath, destinationFilePath);
+        }
+    }
+}
 
 const copy = async () => {
-    // Write your code here 
+   try {
+    await fs.access(fromFolder);
+    await fs.access(toFolder);
+    throw Error('Destination folder already exists')
+   } catch (error) {
+    if (error.code === 'ENOENT') {
+        try {
+            await fs.mkdir(toFolder);
+            await copyFileOrFolder(fromFolder, toFolder);
+        } catch (error) {
+            throw Error('FS operation failed')
+        }
+    } else {
+        throw Error('FS operation failed')
+    }
+   }
 };
 
 await copy();
