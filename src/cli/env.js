a27384cac64b90a76dc584c3da3af770a1774e92
dmirty.s <d.strizhakov@gmail.com>
2024-01-27 15:40:21 +0300

    feat: add solition to compress and decompress file

--- src/cli/env.js
+++ src/cli/env.js
@@ -5,7 +5,6 @@ const parseEnv = () => {
            console.log(`${field}=${process.env[field]}`)
        }
    }
-
 };
 
 parseEnv();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,30 @@
+import path from 'node:path';
+import { createUnzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import {
+    createReadStream,
+    createWriteStream,
+} from 'node:fs';
+
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+    const pathToFile = path.join(__dirname, '/files/fileToCompress.txt');
+    const pathToCompressedFile = path.join(__dirname, '/files/archive.txt.gz');
+
+    const unzip = createUnzip();
+    const source = createReadStream(pathToCompressedFile);
+    const destination = createWriteStream(pathToFile);
+
+    pipeline(source, unzip, destination, (error) => {
+        if (error) {
+            console.error('An error occurred:', error);
+            process.exitCode = 1;
+        }
+    })
 };
 
 await decompress();
